9e1c33a4d23f01a582cc2d8a359f434977639c8e
kkamdung <kkamdung@empal.com>
2026-07-06 20:32:20 +0900

    접수된 주문에 대한 포장 함수 빈 설정 추가

---
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
@@ -0,0 +1,21 @@
+package com.polarbookshop.dispatcherservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Slf4j
+@Configuration
+public class DispatchingFunctions {
+
+    @Bean
+    public Function<OrderAcceptedMessage, Long> pack() {
+        return orderAcceptedMessage -> {
+            log.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
+            return orderAcceptedMessage.orderId();
+        };
+    }
+
+}
